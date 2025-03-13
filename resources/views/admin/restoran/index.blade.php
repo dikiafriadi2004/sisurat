cbc8f950b95f305efcedefde7a6db8405b4749cf
Diki Afriadi <diki.afriadi2004@gmail.com>
2025-03-13 10:44:09 +0700

    updated

--- resources/views/admin/restoran/index.blade.php
+++ resources/views/admin/restoran/index.blade.php
@@ -15,8 +15,7 @@
                     <div class="card-body">
                         @if (Session::has('success'))
                             <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                 {{ session('success') }}
                             </div>
                         @endif
